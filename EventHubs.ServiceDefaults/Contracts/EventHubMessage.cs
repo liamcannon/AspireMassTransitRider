e88bfa86c2c81ccb22c10ca878bb68b0203943ac
cannonliam_grmn <Liam.Cannon@garmin.com>
2025-08-20 14:54:12 -0400

    fixed project refs

--- EventHubs.ServiceDefaults/Contracts/EventHubMessage.cs
+++ EventHubs.ServiceDefaults/Contracts/EventHubMessage.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EventHubs.ServiceDefaults.Contracts;
+﻿namespace EventHubs.ServiceDefaults.Contracts;
 
 public record EventHubMessage
 {
-    public string Text { get; init; }
+    public string Text { get; set; } = null!;
 }
