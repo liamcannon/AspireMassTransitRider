5327064790492e82461b5e3760b236e919b299d6
cannonliam_grmn <Liam.Cannon@garmin.com>
2025-08-20 15:01:28 -0400

    nit

--- EventHubs.ApiService/Program.cs
+++ EventHubs.ApiService/Program.cs
@@ -5,13 +5,10 @@ const string eventHubName = "hub";
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
 
-// Add services to the container.
 builder.Services.AddProblemDetails();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
